bfe4bb8611248b154ab37a8f1c8bf9da9587c3e1
Rhythm Varshney <rhythm-design>
2021-08-14 16:27:42 +0530

    navbar-color changed

--- src/NavbarReact/NavbarReact.jsx
+++ src/NavbarReact/NavbarReact.jsx
@@ -1,31 +1,49 @@
-import { Nav, Navbar,NavDropdown,Container } from 'react-bootstrap';
-import React from 'react';
-import "./NavbarReact.css";
-import logo from "../Images/bot.png";
+import React from "react";
+import { Navbar, Nav, NavDropdown, Container, Button } from "react-bootstrap";
+import "./NavbarReact.css"
+import Logo from "../images/bot.png";
+import "../styles.css"
 
 function NavbarReact(){
-    return (
-      <Navbar className="navbar-custom-color" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home"><img src={logo} width="45" height="45"></img></Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home"><h4 className="navTextColor">Home</h4></Nav.Link>
-        <Nav.Link href="#link"><h4 className="navTextColor">Link</h4></Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+    return(
+      <>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet"></link>   
+      <Navbar className="navbar-custom-color navbar-text" expand="lg">
+        <Container>
+          <Navbar.Brand className="text-white" href="#home">
+                <img
+              src={Logo}
+              width="40"
+              height="40"
+              className="d-inline-block align-top"
+              alt="logo"
+            />NAVIGATEBOT
+            </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto" >
+            <NavDropdown title="Algorithms" className="text-white basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Dijkstra's Algorithm</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#speed" className="text-white">Speed</Nav.Link>
+              <button>Let's Navigate</button>
+              <Nav.Link href="#clear-board" className="text-white">CLEAR BOARD</Nav.Link>
+              <Nav.Link href="#clear-path" className="text-white">CLEAR PATH</Nav.Link>
+              <Nav.Link href="#clear-walls" className="text-white">CLEAR WALLS</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+        <link
+        rel="stylesheet"
+        href="NavbarReact.css"
+      />
+      </Navbar>
+    </>
     );
-};
 
+}
 
 export default NavbarReact;
+
+
+// https://www.linkpicture.com/q/Navigate-Bot-_1.svg"
